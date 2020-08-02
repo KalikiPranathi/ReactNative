ee5f4f0068ffe33ecf2c777b6e2764ba68155d98
DESKTOP-N17VTE8\Kaliki Pranathi <pranathithaara@gmail.com>
2020-08-02 11:31:03 +0530

    navigation2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
-import Dishdetail from './DishDetailComponent'
+import Dishdetail from './DishDetailComponent';
+import Home from './HomeComponent';
 import { View, Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
+import {createDrawerNavigator} from 'react-navigation-drawer';
+import { Icon } from 'react-native-elements';
+
+const HomeNavigator = createStackNavigator({
+  Home: { screen: Home }
+}, {
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+        backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+        color: "#fff"            
+    },
+    headerTintColor: "#fff"  
+  })
+});
+
 const MenuNavigator = createStackNavigator({
   Menu: { screen: Menu },
   Dishdetail: { screen: Dishdetail }
@@ -19,9 +37,27 @@ const MenuNavigator = createStackNavigator({
           color: "#fff"            
       }
   }
-}
-);
-const MenuNavigatorContainer = createAppContainer(MenuNavigator);
+});
+
+const MainNavigator = createDrawerNavigator({
+  Home: 
+    { screen: HomeNavigator,
+      navigationOptions: {
+        title: 'Home',
+        drawerLabel: 'Home'
+      }
+    },
+  Menu: 
+    { screen: MenuNavigator,
+      navigationOptions: {
+        title: 'Menu',
+        drawerLabel: 'Menu'
+      }, 
+    }
+}, {
+drawerBackgroundColor: '#D1C4E9'
+});
+const MenuNavigatorContainer = createAppContainer(MainNavigator);
 class Main extends Component {  
 
   render() {
