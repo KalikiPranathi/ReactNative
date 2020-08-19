71a1f52fbeeeac97b6e1c8778b698b7c29bad353
DESKTOP-N17VTE8\Kaliki Pranathi <pranathithaara@gmail.com>
2020-08-19 12:53:56 +0530

    social sharing

--- components/DishDetailComponent.js
+++ components/DishDetailComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList,StyleSheet,Modal,Button,Alert, PanResponder } from 'react-native';
+import { Text, View, ScrollView, FlatList,StyleSheet,Modal,Button,Alert, PanResponder,Share } from 'react-native';
 import { Card, Icon,Rating,Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -92,6 +92,15 @@ function RenderDish(props) {
             return true;
         },
     })
+    const shareDish = (title, message, url) => {
+        Share.share({
+            title: title,
+            message: title + ': ' + message + ' ' + url,
+            url: url
+        },{
+            dialogTitle: 'Share ' + title
+        })
+    }
         if (dish != null) {
             return(
                 <Animatable.View animation="fadeInDown" duration={2000} delay={1000} {...panResponder.panHandlers} ref={handleViewRef}>
@@ -117,6 +126,15 @@ function RenderDish(props) {
                             type='font-awesome'
                             color="#512DA8"
                             onPress={() => props.toggleModal()}
+                        />
+                        <Icon
+                            raised
+                            reverse
+                            name='share'
+                            type='font-awesome'
+                            color='#51D2A8'
+                            style={styles.cardItem}
+                            onPress={() => shareDish(dish.name, dish.description, baseUrl + dish.image)} 
                         />
                     </View>
                 </Card>
